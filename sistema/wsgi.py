126552fb37572394104706d9af5a6a97c8bfbf5b
Junior <juniorfarias10@icloud.com>
2015-05-14 01:52:34 -0300

    correcao

--- sistema/wsgi.py
+++ sistema/wsgi.py
@@ -8,11 +8,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os,sys
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
-if PROJECT_ROOT not in sys.path:
-    sys.path.append(PROJECT_ROOT)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistema.settings")
